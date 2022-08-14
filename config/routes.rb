3bb41e30b138619c5a8508b1c69c0c9594b0b83c
Renan Martins <renanmartins989@gmail.com>
2022-08-15 00:02:27 +0100

    routes done with pages#home as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :reviews, only: :create
+  end
+
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
