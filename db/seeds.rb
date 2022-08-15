e35f2b8b517165e6d6db5516c342265fce897aa5
Renan Martins <renanmartins989@gmail.com>
2022-08-15 02:23:37 +0100

    movies API working good, removing some bootstrap from the views, problem with new movie button, i dont know exactly why yet, something about this be a simple_form input button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts 'Destroying current database...'
+
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+
+puts 'Creating new movie database via API...'
+
+response['results'].each do |movie_hash|
+  puts
+  p movie_hash
+  Movie.create!(
+    title: movie_hash['title'],
+    overview: movie_hash['overview'],
+    poster_url: movie_hash['poster_path'],
+    rating: movie_hash['vote_average']
+  )
+
+end
+
+puts 'New database created!'
