0836a308fb656382cf91d70581dc988c9c963ff4
Renan Martins <renanmartins989@gmail.com>
2022-08-20 00:45:04 +0100

    removing select2, get error with heroku. a few adjusts index cards

--- app/javascript/controllers/select2_controller.js
+++ app/javascript/controllers/select2_controller.js
@@ -1,12 +1,12 @@
-import { Controller } from "@hotwired/stimulus";
-import $ from "jquery";
-import "select2";
-import "select2/dist/css/select2.min.css";
+// import { Controller } from "@hotwired/stimulus";
+// import $ from "jquery";
+// import "select2";
+// import "select2/dist/css/select2.min.css";
 
-export default class extends Controller {
-  connect() {
-    $(this.element).select2({
-      width: "100%"
-    });
-  }
-}
+// export default class extends Controller {
+//   connect() {
+//     $(this.element).select2({
+//       width: "100%"
+//     });
+//   }
+// }
