c112de96dc96611469e216075ac007fb9c571877
Renan Martins <renanmartins989@gmail.com>
2022-08-23 03:19:54 +0100

    remove the js file and folders that was not being in use, install sortable to drag and drop the cards, maybe movies as well

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 // Bootstrap import
 import "bootstrap";
 
+// Components import
+// import {  } from "module";
 // Stimulus import
 // import { Application } from "stimulus"
 // import { definitionsFromContext } from "stimulus/webpack-helpers"
@@ -22,5 +24,5 @@ import "bootstrap";
 
 // Stimulus
 // const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
+// const context = require.context("controllers", true, /_controller\.js$/)
 // application.load(definitionsFromContext(context))
